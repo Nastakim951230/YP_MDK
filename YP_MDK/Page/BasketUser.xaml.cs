80877536820748279b049dc284a1d1d30ba81d06
Настя Ким <nastytrifono03@gmail.com>
2023-03-14 18:08:23 +0300

    корзина и вывод талона

--- YP_MDK/Page/BasketUser.xaml.cs
+++ YP_MDK/Page/BasketUser.xaml.cs
@@ -24,6 +24,7 @@ namespace YP_MDK
         double summaCoint;
         double summaSkidki;
         List<ClassProductBasket> baskets;
+        string itogskidka;
         List <PointOfIssue> points=ClassPage.ClassBase.BD.PointOfIssue.ToList();
         public BasketUser(User user, List<ClassProductBasket> baskets)
         {
@@ -56,7 +57,7 @@ namespace YP_MDK
                 }
             }
             double skidka = (summaSkidki - summaCoint) * 100 / summaSkidki;
-            string itogskidka = string.Format("{0:F2}", skidka);
+             itogskidka = string.Format("{0:F2}", skidka);
             ItogCoint.Text = "Итоговая цена: " + summaCoint;
             ItogSkidka.Text = "Итоговая скидка: " + itogskidka + "%";
         }
@@ -93,18 +94,212 @@ namespace YP_MDK
 
         private void order_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                List<Product> products = ClassPage.ClassBase.BD.Product.ToList();
+               DateTime date= DateTime.Today;
+                string dateDelivery = "";
+                bool position = false;
+                for(int i = 0; i < baskets.Count; i++)
+                {
+                    Product product = products.FirstOrDefault(x => x.ProductArticleNumber == baskets[i].productBasket.ProductArticleNumber);
+                    if (product.ProductQuantityInStock<=3)
+                    {
+                       position = true;
+                    }
+                   
+                }
+                if(position==true)
+                {
+                   date= date.AddDays(6);
+                    dateDelivery = "6 дней";
+                }
+                else
+                {
+                    date = date.AddDays(3);
+                    dateDelivery = "3 дня";
+                }
+                List<Order> kodOrder = ClassPage.ClassBase.BD.Order.ToList();
+                int kolvoProductov=kodOrder.Count;
+                int kod = (kodOrder[kolvoProductov-1].CodeToReceive)+1;
 
+               perehod: List<Order> orders = kodOrder.Where(x => x.CodeToReceive == kod && x.OrderStatus==1).ToList();
+                if(orders.Count>0)
+                {
+                    kod = kod + 1;
+                    goto perehod;
+                }
+                else
+                {
+                    if (address.SelectedIndex > 0)
+                    {
+                        Order order = new Order();
+                        order.OrderStatus = 1;
+                        order.OrderDeliveryDate = DateTime.Today;
+                        order.OrderPickupPoint = address.SelectedIndex;
+                        order.OrderDate = date;
+                        order.CodeToReceive = kod;
+                        if(user==null)
+                        {
+                            order.OrderUser = null;
+                        }
+                        else
+                        {
+                            order.OrderUser = user.UserID;
+                        }
+                        ClassPage.ClassBase.BD.Order.Add(order);
+                       
+
+
+                        for (int i = 0; i < baskets.Count; i++)
+                        {
+                            OrderProduct orderProduct = new OrderProduct();
+                            orderProduct.OrderID=order.OrderID;
+                            orderProduct.ProductArticleNumber = baskets[i].productBasket.ProductArticleNumber;
+                            orderProduct.kolvo = baskets[i].count;
+                            ClassPage.ClassBase.BD.OrderProduct.Add(orderProduct);
+                           
+                        }
+                        ClassPage.ClassBase.BD.SaveChanges();
+                        MessageBox.Show("Заказ успешно создан");
+                        Ticket ticket = new Ticket(order, baskets, summaCoint, itogskidka, dateDelivery);
+                        ticket.ShowDialog();
+                        baskets.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выберить пункт выдачи");
+                    }
+                }
+               
+
+            }
+            catch
+            {
+                MessageBox.Show("При создание заказа возникла ошибка!");
+            }
         }
 
         private void DeletProductBasket_Click(object sender, RoutedEventArgs e)
         {
-
+            ClassProductBasket productBasket = new ClassProductBasket();
+            Button btn = (Button)sender;
+            string id = btn.Uid;
+            if (id != null)
+            {
+                 productBasket=baskets.FirstOrDefault(x=>x.productBasket.ProductArticleNumber == id);
+                baskets.Remove(productBasket);
+                if (baskets.Count > 0)
+                {
+                    ClassPage.FrameNavigate.perehod.Navigate(new BasketUser(user, baskets));
+                }
+                else
+                {
+                    if (user != null)
+                    {
+                        if(baskets.Count==0)
+                        {
+                            baskets = null;
+                        }
+                        if (user.UserRole == 1)
+                        {
+                            Page.Str_Gost.Basket = baskets;
+                            ClassPage.FrameNavigate.perehod.Navigate(new Page.Client(user));
+                        }
+                        else if (user.UserRole == 2)
+                        {
+                            Page.Str_Gost.Basket = baskets;
+                            ClassPage.FrameNavigate.perehod.Navigate(new Page.Admin(user));
+                        }
+                        else if (user.UserRole == 3)
+                        {
+                            Page.Str_Gost.Basket = baskets;
+                            ClassPage.FrameNavigate.perehod.Navigate(new Page.Manager(user));
+                        }
+                    }
+                    else
+                    {
+                        if (baskets.Count == 0)
+                        {
+                            baskets = null;
+                        }
+                        Page.Str_Gost.Basket = baskets;
+                        ClassPage.FrameNavigate.perehod.Navigate(new Page.Str_Gost());
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка");
+            }
         }
 
         private void CountProduct_TextChanged(object sender, TextChangedEventArgs e)
         {
-            ClassProductBasket x=(ClassProductBasket)ListBasket.SelectedItem;
-            
+            ClassProductBasket productBasket = new ClassProductBasket();
+            TextBox btn = (TextBox)sender;
+            string id = btn.Uid;
+            if(id!=null)
+            {
+                productBasket = baskets.FirstOrDefault(x => x.productBasket.ProductArticleNumber == id);
+                if(btn.Text.Replace(" ", "") == "")
+                {
+                    productBasket.count = 0;
+                }
+                else
+                {
+                    
+                    productBasket.count = Convert.ToInt32(btn.Text);
+                    
+                }
+               if(productBasket.count == 0)
+                {
+                    baskets.Remove(productBasket);
+                    if (baskets.Count > 0)
+                    {
+                        ClassPage.FrameNavigate.perehod.Navigate(new BasketUser(user, baskets));
+                    }
+                    else
+                    {
+                        if (user != null)
+                        {
+                            if (baskets.Count == 0)
+                            {
+                                baskets = null;
+                            }
+                            if (user.UserRole == 1)
+                            {
+                                Page.Str_Gost.Basket = baskets;
+                                ClassPage.FrameNavigate.perehod.Navigate(new Page.Client(user));
+                            }
+                            else if (user.UserRole == 2)
+                            {
+                                Page.Str_Gost.Basket = baskets;
+                                ClassPage.FrameNavigate.perehod.Navigate(new Page.Admin(user));
+                            }
+                            else if (user.UserRole == 3)
+                            {
+                                Page.Str_Gost.Basket = baskets;
+                                ClassPage.FrameNavigate.perehod.Navigate(new Page.Manager(user));
+                            }
+                        }
+                        else
+                        {
+                            if (baskets.Count == 0)
+                            {
+                                baskets = null;
+                            }
+                            Page.Str_Gost.Basket = baskets;
+                            ClassPage.FrameNavigate.perehod.Navigate(new Page.Str_Gost());
+                        }
+                    }
+                }
+               
+            }
+            else
+            {
+                MessageBox.Show("Ошибка");
+            }
         }
     }
 }
---
+++ YP_MDK/Ticket.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YP_MDK
+{
+    /// <summary>
+    /// Логика взаимодействия для Ticket.xaml
+    /// </summary>
+    public partial class Ticket : Window
+    {
+        Order order; // 
+        List<ClassProductBasket> baskets;
+        PointOfIssue issues;
+        double summaCoint;
+        string summaDiscount;
+        string dateDelivery;
+        public Ticket(Order order, List<ClassProductBasket> baskets, double summaCoint, string summaDiscount, string dateDelivery)
+        {
+            InitializeComponent();
+            this.order = order;
+            this.baskets = baskets;
+            this.summaCoint = summaCoint;
+            this.summaDiscount = summaDiscount;
+            this.dateDelivery = dateDelivery;
+            NomerZakaza.Text= "Нормер заказа: "+order.OrderID.ToString();
+            dataZakaz.Text = "Дата заказа: " + string.Format("{0:dd MMMM yyyy}", order.OrderDeliveryDate);
+            dataOfReceiving.Text = "Дата получения: " + string.Format("{0:dd MMMM yyyy}", order.OrderDate);
+            day.Text = "Срок доставки: " + dateDelivery;
+            issues = ClassPage.ClassBase.BD.PointOfIssue.FirstOrDefault(x => x.OrderPickupPointID == order.OrderPickupPoint);
+            PickupPoint.Text = "Пункт выдачи: " + issues.Address + "  Индекс пункта: " + issues.Index_zakaz;
+            Kod.Text = "Код получения: " + order.CodeToReceive;
+            Summa.Text = "Итог:"+summaCoint.ToString();
+            Skidka.Text = "Скидка: "+summaDiscount+"%";
+        }
+
+        private void document_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Nazad_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
